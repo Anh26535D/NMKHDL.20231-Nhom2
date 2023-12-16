345eeb5508527621cc8828eb99689e54d857463d
Anh26535D <86581717+Anh26535D@users.noreply.github.com>
2023-12-16 14:52:27 +0700

    edit bat file

--- scripts.bat
+++ scripts.bat
@@ -3,12 +3,12 @@
 setlocal enabledelayedexpansion
 
 rem Display help information when no options are provided
-if "%~1"=="" (
+if "%~1"=="--help" (
     :help
     echo Usage: .\scripts.bat [OPTIONS]
     echo.
     echo Options:
-    echo   /?, -h, --help    Display this help message
+    echo   --help    Display this help message
     echo   crawl_listing     Run the crawl listing companies script
     echo   crawl_prices      Run the crawl prices script
     echo   init_folder       Run the init folder script
@@ -20,35 +20,27 @@ if "%~1"=="" (
     rem Check the provided options using a for loop
     for %%i in (%*) do (
         set "option=%%i"
-        if /I "!option!"=="/?" (
-            goto :help
-        ) else if /I "!option!"=="-h" (
-            goto :help
-        ) else if /I "!option!"=="--help" (
-            goto :help
+        rem Process the specified option
+        rem Add additional options handling as needed
+        if "!option!"=="crawl_listing" (
+            python .\scripts\run_crawl_listing_companies.py
+        ) else if "!option!"=="crawl_prices" (
+            python .\scripts\run_crawl_prices.py
+        ) else if "!option!"=="init_folder" (
+            python .\scripts\run_init_folder.py
+        ) else if "!option!"=="train_model" (
+            python .\scripts\run_train_model.py
+        ) else if "!option!"=="transform_prices" (
+            python .\scripts\run_transform_prices.py
+        ) else if "!option!"=="all" (
+            python .\scripts\run_crawl_listing_companies.py
+            python .\scripts\run_crawl_prices.py
+            python .\scripts\run_init_folder.py
+            python .\scripts\run_train_model.py
+            python .\scripts\run_transform_prices.py
         ) else (
-            rem Process the specified option
-            rem Add additional options handling as needed
-            if "!option!"=="crawl_listing" (
-                python .\scripts\run_crawl_listing_companies.py
-            ) else if "!option!"=="crawl_prices" (
-                python .\scripts\run_crawl_prices.py
-            ) else if "!option!"=="init_folder" (
-                python .\scripts\run_init_folder.py
-            ) else if "!option!"=="train_model" (
-                python .\scripts\run_train_model.py
-            ) else if "!option!"=="transform_prices" (
-                python .\scripts\run_transform_prices.py
-            ) else if "!option!"=="all" (
-                python .\scripts\run_crawl_listing_companies.py
-                python .\scripts\run_crawl_prices.py
-                python .\scripts\run_init_folder.py
-                python .\scripts\run_train_model.py
-                python .\scripts\run_transform_prices.py
-            ) else (
-                echo Invalid argument: !option!
-            )
-        )
+            echo Invalid argument: !option!
+        )  
     )
 )
 
